b3e80218f487bcb6a9edbaf92d6ce2e3b2c7019d
Tallián Balázs <tebihun@gmail.com>
2020-05-31 20:32:48 +0200

    added multiple language support

--- hello_test.go
+++ hello_test.go
@@ -6,20 +6,26 @@ func TestHello(t *testing.T) {
 
 	assertCorrectMessage := func(t *testing.T, got, want string) {
 		t.Helper()
-		if got !=want {
+		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
 	}
 
-	t.Run("saying hello to people", func(t *testing.T){
-		got := Hello("Balu")
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Balu", "")
 		want := "Hello, Balu"
 		assertCorrectMessage(t, got, want)
 	})
 
-	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T){
-		got := Hello("")
+	t.Run("say 'Hello, World' if argument is empty", func(t *testing.T) {
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 }
